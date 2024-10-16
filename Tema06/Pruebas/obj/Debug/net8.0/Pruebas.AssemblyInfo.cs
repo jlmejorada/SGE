c548cb0cb47f2f312185e6871036fa2110ccd103
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-10-16 13:38:44 +0200

    Subida boletin01

--- Tema06/Pruebas/obj/Debug/net8.0/Pruebas.AssemblyInfo.cs
+++ Tema06/Pruebas/obj/Debug/net8.0/Pruebas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pruebas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f8214015e6ac3f772e7e4733647e6003bb06977")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pruebas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pruebas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
